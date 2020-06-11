0ca95389154adf0625b656d6a2c1980e9c8aa8bc
Erik <erikquispechoque@gmail.com>
2020-06-11 12:34:15 -0300

    Pacrcial Terminado v2

--- src/Lib.hs
+++ src/Lib.hs
@@ -134,8 +134,8 @@ pagar turista tour = modificarStress (+(length tour)) turista
 
 --parte b
 
-tourConvicente::Tour->Turista->Bool
-tourConvicente tour turista =any (condicionConvincente turista) tour
+tourConvicente::Turista->Tour->Bool
+tourConvicente turista tour  =any (condicionConvincente turista) tour
 
 condicionConvincente::Turista -> Excursion->Bool
 condicionConvincente turista escursion = esDesestresante turista escursion  &&  dejaAcompañado turista escursion
@@ -145,12 +145,11 @@ dejaAcompañado turista excursion = (not.viajaSolo) (hacerExcursion turista excu
 
 --parte c
 saberEfectividad::[Turista]->Tour->Int
-saberEfectividad turistas tour = sum (map (espiritualidad tour) turistas)
+saberEfectividad turistas tour = sum . map (espiritualidad tour ) $ turistas
 
-
+--falta implementar tour convincente filter (tourConvicente turista) tour
 espiritualidad ::Tour->Turista->Int
-espiritualidad tour turista = sum (map (perdidas2Indices nivelStress nivelCansancio turista) tour)
-
+espiritualidad tour turista = sum . map (perdidas2Indices nivelStress nivelCansancio turista) $ tour
 perdidas2Indices::Indice->Indice->Turista->Excursion->Int
 perdidas2Indices indice1 indice2 turista excursion = abs (deltaExcursionSegun indice1 turista excursion) + abs(deltaExcursionSegun indice2 turista excursion)
 
